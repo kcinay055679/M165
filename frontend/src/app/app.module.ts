a887688b4ee9a53eb1723528f18fc59845faddc2
megli2 <egli@puzzle.ch>
2023-06-22 06:13:10 +0200

    create landing page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,10 +6,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
